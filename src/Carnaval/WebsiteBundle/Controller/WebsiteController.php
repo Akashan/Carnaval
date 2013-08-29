64f13bada325712569674bc731c81b6fd853681e
Akashan <akashan31@gmail.com>
2013-08-29 18:19:08 +0200

    suppression démo + création des vues (vides)

--- src/Carnaval/WebsiteBundle/Controller/WebsiteController.php
+++ src/Carnaval/WebsiteBundle/Controller/WebsiteController.php
@@ -18,47 +18,47 @@ class WebsiteController extends Controller
 	
 	public function referencesAction()
 	{
-	
-	}
+        return $this->render('CarnavalWebsiteBundle:Website:references.html.twig', array());
+    }
 	
 	public function catalogueAction()
 	{
-	
+        return $this->render('CarnavalWebsiteBundle:Website:catalogue.html.twig', array());
 	}
 	
 	public function calendrierAction()
 	{
-	
+        return $this->render('CarnavalWebsiteBundle:Website:calendrier.html.twig', array());
 	}
 	
 	public function galeriesAction()
 	{
-	
+        return $this->render('CarnavalWebsiteBundle:Website:galeries.html.twig', array());
 	}
 	
 	public function voirGaleriesAction($id)
 	{
-	
+        return $this->render('CarnavalWebsiteBundle:Website:voirGalerie.html.twig', array());
 	}
 	
 	public function videosAction()
 	{
-	
+        return $this->render('CarnavalWebsiteBundle:Website:videos.html.twig', array());
 	}
 	
 	public function voirVideosAction($id)
 	{
-	
+        return $this->render('CarnavalWebsiteBundle:Website:voirVideo.html.twig', array());
 	}
 	
 	public function contactsAction()
 	{
-	
+        return $this->render('CarnavalWebsiteBundle:Website:contacts.html.twig', array());
 	}
 	
 	public function liensAction()
 	{
-	
+        return $this->render('CarnavalWebsiteBundle:Website:liens.html.twig', array());
 	}
 	
 }
